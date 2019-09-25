5fef55bb54e5f062be2396b679c481d77b7eaf46
Joao Correia <joao.correia@pitang.com>
2019-09-25 19:42:59 -0300

    Mudando o banco de dados de postgres para H2 em memoria.

--- src/main/java/br/com/treino/gerenciadortarefas/GerenciadorTarefasApplication.java
+++ src/main/java/br/com/treino/gerenciadortarefas/GerenciadorTarefasApplication.java
@@ -1,13 +1,21 @@
 package br.com.treino.gerenciadortarefas;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import br.com.treino.gerenciadortarefas.modelos.User;
+import br.com.treino.gerenciadortarefas.repositorios.RepositorioUsuario;
 
 @SpringBootApplication
 public class GerenciadorTarefasApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GerenciadorTarefasApplication.class, args);
 	}
+
+
 
 }
